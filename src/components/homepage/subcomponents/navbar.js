8d24686ba4dd191cb675279e66e8ea4ca20b2dab
Francesco <francesco.mazzini432@gmail.com>
2023-01-02 22:34:37 +0100

    started customizing the navbar
    Signed-off-by: Francesco <francesco.mazzini432@gmail.com>

--- src/components/homepage/subcomponents/navbar.js
+++ src/components/homepage/subcomponents/navbar.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import "../../../assets/css/bootstrap.min.css";
 import "../../../assets/css/bootstrap-override.css";
 import "../../../assets/css/style.css";
+import Menu from '@mui/icons-material/Menu' ;
 
 export const Navbar = () => {
     
@@ -11,7 +12,7 @@ export const Navbar = () => {
             <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
                 <a className="navbar-brand font-josefin normal-text pl-3 p-3 mr-2 ml-2" href="#"> ❄ HOME </a>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
+                    <span ><Menu /></span>
                 </button>
 
                 <div className="collapse navbar-collapse " id="navbarSupportedContent">
